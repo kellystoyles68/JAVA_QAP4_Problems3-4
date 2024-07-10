4166d5796463f91fda9a3368644652f558806952
kellystoyles68 <150394889+kellystoyles68@users.noreply.github.com>
2024-07-10 07:54:09 -0230

    commit comments

--- ParseInts.java
+++ ParseInts.java
@@ -3,20 +3,26 @@ import java.util.Scanner;
 public class ParseInts {
     public static void main(String[] args)
     {
-	
+	//initialize sum
     int sum = 0;
+
+    //create scanner
 	Scanner scan = new Scanner(System.in);
 	
-
+    //prompt user to enter information
 	System.out.println("Enter a line of text");
+
+    //create new scanner
 	Scanner scanLine = new Scanner(scan.nextLine());
 
+    //loop through each line
 	while (scanLine.hasNext())
 	    try{		
         sum += Integer.parseInt(scanLine.next());
         } catch (NumberFormatException e){
-           
+           // if not a valid integer, catch the exception
     }
+    //print out sum of integers
     System.out.println("The sum of the integers on this line is " + sum);
 	}
     }
